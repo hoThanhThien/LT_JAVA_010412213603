24701561dc051022f2d77560a39b7be40fea8cd4
sauvole565 <71451891+caophi562005@users.noreply.github.com>
2025-03-06 17:01:31 +0700

    design header và footer

---
+++ LaundryShop/src/components/Footer/Footer.tsx
@@ -0,0 +1,102 @@
+import Logo from "@/assets/heramo-logo.png";
+import { Link } from "react-router";
+import fb from "@/assets/footer/fb-icon.svg";
+import zalo from "@/assets/footer/zalo-icon.svg";
+import ins from "@/assets/footer/ins-icon.svg";
+import youtube from "@/assets/footer/youtube-icon.svg";
+import linkedin from "@/assets/footer/linkedin-icon.svg";
+
+export default function Footer() {
+  return (
+    <footer className="bg-black w-full py-12 text-xl text-gray-400 leading-loose">
+      <div className="w-full mx-auto lg:max-w-6xl">
+        <div className="text-white">Liên hệ</div>
+        <span>Hotline : </span>
+        <span className="text-white">19003286</span>
+        <div>HERAMO JSC | GPKD /MST: 0314624471</div>
+        <div>
+          Văn phòng đại diện: 01 Trường Sơn, Phường 4, Quận Tân Bình, HCM
+        </div>
+        <div>CN1: 01 Trường Sơn, Phường 4, Quận Tân Bình, HCM</div>
+        <div>CN2: 18A/71 Nguyễn Thị Minh Khai, Phường Đa Kao, Quận 1, HCM</div>
+        <div>CN3: 13C1 Hồ Hảo Hớn, Phường Cô Giang, Quận 1, HCM</div>
+        <div>CN4: 197 Nguyễn Văn Lượng, Phường 10, Quận Gò Vấp, HCM</div>
+        <div>CN5: 85 Võ Thị Sáu, Phường 6, Quận 3</div>
+        <div>CN6: 179B, Đường 3/2, Phường 11, Quận 10</div>
+        <div>CN7: 314 Lê Văn Việt, Phường Tân Nhơn Phú B, Quận 9</div>
+        <div>CN8: 15A Đường Số 2, Phường An Khánh, TP.Thủ Đức</div>
+        <div>CN9: 398 Hoàng Diệu, Phường 2, Quận 4, HCM</div>
+        <div>
+          CN10: PARC VILLA Đảo Đại Phước (Swanbay), Nhơn Trạch, Đồng Nai
+        </div>
+        <div className="flex flex-col gap-4">
+          <div>
+            <img className="w-auto h-[50px]" src={Logo} alt="Logo" />
+          </div>
+          <div className="text-white">
+            HERAMO - Hệ thống nhượng quyền & Ứng dụng giặt ủi vệ sinh cao cấp
+            4.0
+          </div>
+          <div className="text-white text-base">
+            HERAMO bắt đầu hành trình từ năm 2017 - với khát khao xây dựng một
+            ứng dụng giúp hàng triệu người có thể đặt các dịch vụ giặt ủi , giặt
+            hấp , vệ sinh giày , vệ sinh nhà cửa , vệ sinh máy lạnh tiện lợi ,
+            từ đó, mọi người sẽ có thêm nhiều thời gian để tận hưởng cuộc sống.
+          </div>
+          <div className="text-white text-base">
+            Sau hơn 6 năm hoạt động và tiên phong ứng dụng công nghệ 4.0, HERAMO
+            tự hào là thương hiệu dẫn đầu trong ngành giặt là, giặt hấp, vệ sinh
+            chăm sóc giày, vệ sinh sofa, nệm, rèm, thảm, vệ sinh máy lạnh tại
+            TP.Hồ Chí Minh với 136.000+ khách hàng tin dùng.
+          </div>
+          <div className="text-white text-base">
+            Tại HERAMO, khách hàng có thể đặt tất cả dịch vụ giặt ủi, vệ sinh
+            chỉ với một chạm duy nhất:
+          </div>
+        </div>
+        <div className="grid lg:grid-cols-4 lg:mt-12">
+          <div className="lg:col-span-1 lg:col-start-1">
+            <div className="text-white">Công ty</div>
+            <div>Về chúng tôi</div>
+            <div>Nhượng quyền thương hiệu</div>
+            <div>Tuyển dụng</div>
+            <div>Trở thành đối tác</div>
+            <div>Blog</div>
+            <div>Điều khoản sử dụng</div>
+          </div>
+          <div className="lg:col-span-1 lg:col-start-2">
+            <div className="text-white">Dịch vụ</div>
+            <div>Giặt sấy</div>
+            <div>Giặt hấp</div>
+            <div>Vệ sinh giày</div>
+            <div>Vệ sinh sofa, nệm, rèm, thảm</div>
+            <div>Vệ sinh máy lạnh</div>
+          </div>
+        </div>
+        <hr className="border-t border-gray-400 my-10" />
+        <div className="grid lg:grid-cols-2">
+          <div className="lg:col-span-1 lg:col-start-1 flex gap-8">
+            <Link to={"/"}>
+              <img src={fb} alt="fb" />
+            </Link>
+            <Link to={"/"}>
+              <img src={zalo} alt="zalo" />
+            </Link>
+            <Link to={"/"}>
+              <img src={ins} alt="inx" />
+            </Link>
+            <Link to={"/"}>
+              <img src={youtube} alt="youtube" />
+            </Link>
+            <Link to={"/"}>
+              <img src={linkedin} alt="linkedin" />
+            </Link>
+          </div>
+          <div className="lg:col-span-1 lg:col-start-2 text-right text-sm">
+            Copyrights 2023 @ HERAMO.All rights reserved
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
