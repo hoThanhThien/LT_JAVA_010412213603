f9d217dcd62ca51a9968631b1e8a342120d7347c
sauvole565 <71451891+caophi562005@users.noreply.github.com>
2025-03-18 13:15:22 +0700

    hoàn thành design quảng cáo app

--- laundry-shop-nextjs/src/app/(public)/layout.tsx
+++ laundry-shop-nextjs/src/app/(public)/layout.tsx
@@ -15,46 +15,6 @@ export default function Layout({
   const [openLogin, setOpenLogin] = useState<boolean>(false);
   return (
     <div className="flex min-h-screen w-full flex-col relative">
-      {/* <header className="fixed top-0 right-0 left-0 z-50 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
-        <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
-          <Link
-            href="#"
-            className="flex items-center gap-2 text-lg font-semibold md:text-base"
-          >
-            <Package2 className="h-6 w-6" />
-            <span className="sr-only">Big boy</span>
-          </Link>
-          <NavItems className="text-muted-foreground transition-colors hover:text-foreground flex-shrink-0" />
-        </nav>
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button
-              variant="outline"
-              size="icon"
-              className="shrink-0 md:hidden"
-            >
-              <Menu className="h-5 w-5" />
-              <span className="sr-only">Toggle navigation menu</span>
-            </Button>
-          </SheetTrigger>
-          <SheetContent side="left">
-            <nav className="grid gap-6 text-lg font-medium">
-              <Link
-                href="#"
-                className="flex items-center gap-2 text-lg font-semibold"
-              >
-                <Package2 className="h-6 w-6" />
-                <span className="sr-only">Big boy</span>
-              </Link>
-
-              <NavItems className="text-muted-foreground transition-colors hover:text-foreground" />
-            </nav>
-          </SheetContent>
-        </Sheet>
-        <div className="ml-auto">
-          <DarkModeToggle />
-        </div>
-      </header> */}
       <header className="fixed top-0 left-0 right-0 z-50 w-full md:h-24 bg-black flex items-center">
         <div className="p-0 mx-auto">
           <nav className="flex py-0 px-11 gap-20">
--- laundry-shop-nextjs/src/app/(public)/page.tsx
+++ laundry-shop-nextjs/src/app/(public)/page.tsx
@@ -270,7 +270,7 @@ export default function Home() {
           </form>
         </div>
       </div>
-      <div className="py-24 max-w-7xl mx-auto px-4">
+      <div className="pt-24 max-w-7xl mx-auto px-4">
         <div className="text-4xl text-center font-bold">
           Truyền thông nói gì về chúng tôi
         </div>
@@ -320,6 +320,43 @@ export default function Home() {
           <button className="bg-black text-white px-9 py-3 rounded-4xl font-bold cursor-pointer w-[30%] ">
             NHẬN TƯ VẤN NGAY
           </button>
+        </div>
+      </div>
+      <div>
+        <div className="grid grid-cols-2 bg-[url('/home/phone-tien-trinh.jpg')] bg-center bg-no-repeat bg-cover h-[1124px] px-24">
+          <div className="col-span-1 col-start-1 self-end">
+            <Image
+              src={"/home/homescreen.png"}
+              alt="home-screen"
+              width="565"
+              height="976"
+              // className="w-[40%] h-[290px] mx-auto"
+            />
+          </div>
+          <div className="col-span-1 col-start-2 self-end h-full flex flex-col justify-center items-center gap-15">
+            <div className="text-4xl font-bold">
+              Tải App Ngay - Ưu Đãi Về Tay!
+            </div>
+            <div className="text-xl mt-2">
+              Đặt tất cả dịch vụ giặt ủi, vệ sinh nhanh chóng. Theo dõi đơn
+              hàng, hình ảnh món đồ mọi lúc mọi nơi. Ưu đãi lên đến 100K dành
+              tặng khách mới và khách hàng thân thiết
+            </div>
+            <div className="flex gap-6">
+              <Image
+                src="/app-store.svg"
+                alt="AppStore"
+                width="200"
+                height="200"
+              />
+              <Image
+                src="/google-play.svg"
+                alt="GooglePlay"
+                width="200"
+                height="200"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </main>
