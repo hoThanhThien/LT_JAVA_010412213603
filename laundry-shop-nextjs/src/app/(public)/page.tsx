57654d9527e066ccea6a7926e685edb4aff021ca
sauvole565 <71451891+caophi562005@users.noreply.github.com>
2025-03-20 11:38:18 +0700

    hoàn thành trang "Về chúng tôi"

--- laundry-shop-nextjs/src/app/(public)/page.tsx
+++ laundry-shop-nextjs/src/app/(public)/page.tsx
@@ -1,8 +1,7 @@
 import Comment from "@/components/comment";
-import News from "@/components/news";
 import Stores from "@/components/stores";
 import Image from "next/image";
-import Link from "next/link";
+import Communication from "@/components/communication";
 
 export default function Home() {
   return (
@@ -14,7 +13,7 @@ export default function Home() {
               Hệ thống nhượng quyền & ứng dụng giặt ủi vệ sinh cao cấp 4.0
             </div>
             <div className="text-xl mt-3">
-              HERAMO (phát âm “hê-ra-mô”) ra đời từ 2017 bởi những chuyên gia
+              HERAMO (phát âm "hê-ra-mô") ra đời từ 2017 bởi những chuyên gia
               hàng đầu trong ngành công nghệ & dịch vụ, với tầm nhìn trở thành
               công ty số một trong ngành giặt ủi vệ sinh cao cấp. Tới nay HERAMO
               đã phát triển trở thành thương hiệu dẫn đầu trong ngành, với 10
@@ -271,57 +270,8 @@ export default function Home() {
           </form>
         </div>
       </div>
-      <div className="pt-24 max-w-7xl mx-auto px-4">
-        <div className="text-4xl text-center font-bold">
-          Truyền thông nói gì về chúng tôi
-        </div>
-        <div className="flex gap-10 justify-center items-center mt-20">
-          <iframe
-            src="https://www.youtube.com/embed/hz7ZNd_wEAY?controls=0&showinfo=0&rel=0&autoplay=1&loop=1"
-            width="40%"
-            height="360px"
-            className="rounded-2xl"
-          />
-          <iframe
-            src="https://www.youtube.com/embed/-jLGktu6Mlc?controls=0&showinfo=0&rel=0&autoplay=1&loop=1"
-            width="40%"
-            height="360px"
-            className="rounded-2xl"
-          />
-        </div>
-        <div className="relative mt-5">
-          <Link
-            target="_blank"
-            href={
-              "https://cafebiz.vn/heramo-ung-dung-giat-ui-ve-sinh-40-dat-top-3-startup-wheel-2021-20211115122448562.chn"
-            }
-          >
-            <Image
-              src={"/home/top3-startup.png"}
-              alt="top3"
-              width="0"
-              height="0"
-              sizes="100vw"
-              className="w-[40%] h-[290px] mx-auto"
-            />
-            <Image
-              src={"/home/top3-startup-text.png"}
-              alt="top3-text"
-              width="311"
-              height="23"
-              sizes="100vw"
-              className="absolute left-0 right-0 bottom-8 mx-auto max-w-[90%]"
-            />
-          </Link>
-        </div>
-        <div className="mt-10">
-          <News />
-        </div>
-        <div className="flex justify-center items-center mt-10">
-          <button className="bg-black text-white px-9 py-3 rounded-4xl font-bold cursor-pointer w-[30%] ">
-            NHẬN TƯ VẤN NGAY
-          </button>
-        </div>
+      <div>
+        <Communication />
       </div>
       <div>
         <div className="grid grid-cols-2 bg-[url('/home/phone-tien-trinh.jpg')] bg-center bg-no-repeat bg-cover h-[1124px] px-24">
--- laundry-shop-nextjs/src/app/(public)/ve-chung-toi/page.tsx
+++ laundry-shop-nextjs/src/app/(public)/ve-chung-toi/page.tsx
@@ -1,4 +1,6 @@
+import Communication from "@/components/communication";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function VeChungToi() {
   return (
@@ -218,6 +220,232 @@ export default function VeChungToi() {
                 </div>
               </div>
             </div>
+          </div>
+        </div>
+      </div>
+      <div className="bg-gray-100 py-24 px-[300px]">
+        <div className="text-4xl font-medium text-center mb-20">
+          Những con số biết nói
+        </div>
+        <div className="w-[900px]max-w-full flex flex-wrap justify-center gap-10">
+          <div className="w-[30%] flex gap-2">
+            <div>
+              <Image
+                src="/ve-chung-toi/kh.png"
+                alt="kh"
+                width="120"
+                height="68"
+                className="max-w-[120px]"
+              />
+            </div>
+            <div>
+              <div className="text-lg font-bold">136.000+</div>
+              <div className="text-gray-600 text-sm mt-1">
+                Số khách hàng tin yêu và lựa chọn
+              </div>
+            </div>
+          </div>
+          <div className="w-[30%] flex gap-2">
+            <div>
+              <Image
+                src="/ve-chung-toi/dc.png"
+                alt="kh"
+                width="120"
+                height="68"
+                className="max-w-[120px]"
+              />
+            </div>
+            <div>
+              <div className="text-lg font-bold">420.000+</div>
+              <div className="text-gray-600 text-sm mt-1">
+                Tổng số món đồ đã từng vệ sinh chăm sóc
+              </div>
+            </div>
+          </div>
+          <div className="w-[30%] flex gap-2">
+            <div>
+              <Image
+                src="/ve-chung-toi/smile.png"
+                alt="kh"
+                width="120"
+                height="68"
+                className="max-w-[120px]"
+              />
+            </div>
+            <div>
+              <div className="text-lg font-bold">99.7%</div>
+              <div className="text-gray-600 text-sm mt-1">
+                Tỉ lệ khách hàng hài lòng
+              </div>
+            </div>
+          </div>
+          <div className="w-[30%] flex gap-2">
+            <div>
+              <Image
+                src="/ve-chung-toi/brand.png"
+                alt="kh"
+                width="120"
+                height="68"
+                className="max-w-[120px]"
+              />
+            </div>
+            <div>
+              <div className="text-lg font-bold">100+</div>
+              <div className="text-gray-600 text-sm mt-1">
+                Số việc làm đã tạo ra
+              </div>
+            </div>
+          </div>
+          <div className="w-[30%] flex gap-2">
+            <div>
+              <Image
+                src="/ve-chung-toi/tick.png"
+                alt="kh"
+                width="120"
+                height="68"
+                className="max-w-[120px]"
+              />
+            </div>
+            <div>
+              <div className="text-lg font-bold">6+</div>
+              <div className="text-gray-600 text-sm mt-1">
+                Số năm kinh nghiệm
+              </div>
+            </div>
+          </div>
+          <div className="w-[30%] flex gap-2">
+            <div>
+              <Image
+                src="/ve-chung-toi/km.png"
+                alt="kh"
+                width="120"
+                height="68"
+                className="max-w-[120px]"
+              />
+            </div>
+            <div>
+              <div className="text-lg font-bold">702.500km</div>
+              <div className="text-gray-600 text-sm mt-1">
+                Quãng đường shipper di chuyển để giao nhận
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <Communication />
+      </div>
+      <div className="max-w-7xl mx-auto px-24 pb-20">
+        <div className="grid lg:grid-cols-4 gap-10 my-24">
+          <div className="lg:col-span-1 lg:col-start-1 text-[28px] font-bold">
+            Tầm nhìn
+          </div>
+          <div className="lg:col-span-3 lg:col-start-2 text-gray-600">
+            <div>
+              Trong 10 năm tới, HERAMO sẽ trở thành một trong những nền tảng
+              công nghệ & nhượng quyền dịch vụ về đời sống (lifestyle) & nhà cửa
+              (home service) lớn nhất Đông Nam Á - giúp đỡ hàng chục triệu khách
+              hàng các công việc giặt ủi, vệ sinh không gian sống, chăm sóc gia
+              đình để họ có thêm thật nhiều thời gian dành cho những người thân
+              yêu, trải nghiệm hạnh phúc trọn vẹn và nâng cao chất lượng cuộc
+              sống.
+            </div>
+          </div>
+        </div>
+        <div className="grid lg:grid-cols-4 gap-10">
+          <div className="lg:col-span-1 lg:col-start-1 text-[28px] font-bold">
+            Văn hóa công ty
+          </div>
+          <div className="lg:col-span-3 lg:col-start-2 text-gray-600 flex flex-col gap-5">
+            <div>
+              Ở HERAMO, tập thể nhân viên luôn đặt trải nghiệm khách hàng làm
+              trung tâm của mọi quyết định & không ngừng nỗ lực phụng sự theo 6
+              giá trị cốt lõi:
+            </div>
+            <div className="flex flex-col gap-5">
+              <div>
+                <span className="text-lg text-gray-600 font-medium">
+                  H - HONEST (TRUNG THỰC):
+                </span>{" "}
+                Làm đúng những gì đã cam kết với khách hàng
+              </div>
+              <div>
+                <span className="text-lg text-gray-600 font-medium">
+                  E - EXCELLENT (XUẤT SẮC):
+                </span>{" "}
+                Chỉ chấp nhận chất lượng công việc mang đến dịch vụ xuất sắc
+              </div>
+              <div>
+                <span className="text-lg text-gray-600 font-medium">
+                  R - RESULT-ORIENTED (HƯỚNG ĐẾN KẾT QUẢ):
+                </span>{" "}
+                Sự hài lòng của khách hàng là thứ duy nhất đo lường thành quả
+                công việc
+              </div>
+              <div>
+                <span className="text-lg text-gray-600 font-medium">
+                  A - ACCOUNTABLE (TRÁCH NHIỆM):
+                </span>{" "}
+                Chịu trách nhiệm cho mọi hành động và dịch vụ mang đến khách
+                hàng
+              </div>
+              <div>
+                <span className="text-lg text-gray-600 font-medium">
+                  M - MOTIVATED (ĐỘNG LỰC):
+                </span>{" "}
+                Phục vụ khách hàng với nhiệt huyết, động lực cao nhất
+              </div>
+              <div>
+                <span className="text-lg text-gray-600 font-medium">
+                  O - OPEN (CỞI MỞ):
+                </span>{" "}
+                Sẵn sàng lắng nghe mọi ý kiến phản hồi với tinh thần cầu thị và
+                không ngừng cải tiến để trở nên xuất sắc hơn
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-around mt-20">
+          <div>
+            <Image
+              src="/ve-chung-toi/tam_nhin.jpg"
+              alt="Why us"
+              width="343"
+              height="217"
+              className="rounded-[20px] max-w-[343px]"
+            />
+          </div>
+          <div>
+            <Image
+              src="/ve-chung-toi/cot_loi.jpg"
+              alt="Why us"
+              width="343"
+              height="217"
+              className="rounded-[20px] max-w-[343px]"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="bg-main h-fit">
+        <div className="grid grid-cols-2">
+          <div className="flex flex-col justify-center items-center">
+            <div className="text-[26px] font-bold mb-10">
+              Cơ hội nghề nghiệp
+            </div>
+            <Link href="/tuyen-dung">
+              <button className="flex items-center gap-2 bg-transparent border-2 border-black px-9 py-2 rounded-[28px] font-bold cursor-pointer">
+                Tới trang tuyển dụng
+                <Image src="/arrow.svg" alt="Arrow" width={20} height={20} />
+              </button>
+            </Link>
+          </div>
+          <div className="flex justify-center items-center">
+            <Image
+              src="/ve-chung-toi/job.png"
+              alt="job"
+              width="246"
+              height="295"
+            />
           </div>
         </div>
       </div>
