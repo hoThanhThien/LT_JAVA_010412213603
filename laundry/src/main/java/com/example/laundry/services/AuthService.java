3ed186dc754db4f8fc9e5abab07055d92683193c
hoThanhThien <154042780+hoThanhThien@users.noreply.github.com>
2025-03-31 03:21:35 +0700

    Delete bug

--- laundry/src/main/java/com/example/laundry/services/AuthService.java
+++
@@ -1,9 +0,0 @@
-package com.example.laundry.services;
-
-import com.example.laundry.dto.LoginRequest;
-import com.example.laundry.dto.LoginResponse;
-
-public interface AuthService {
-  LoginResponse login(LoginRequest loginRequest);
-  boolean logout(String token);
-}
--- laundry/src/main/java/com/example/laundry/services/ReportService.java
+++
@@ -1,7 +0,0 @@
-package com.example.laundry.services;
-
-import com.example.laundry.models.report.Report;
-
-public interface ReportService {
-//    Report generateReport(String shopName, String reportType, String startDate, String endDate);
-}
